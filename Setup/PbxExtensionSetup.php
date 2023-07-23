911ef5b085cadc79c9f41b9bb81f4320814431e3
nb <nbek@miko.ru>
2023-07-24 00:16:40 +0800

    Fix installation script

--- Setup/PbxExtensionSetup.php
+++ Setup/PbxExtensionSetup.php
@@ -31,67 +31,6 @@ class PbxExtensionSetup extends PbxExtensionSetupBase
 {
 
     /**
-     * PbxExtensionSetup constructor.
-     *
-     * @param string $moduleUniqueID - the unique module identifier
-     */
-    public function __construct(string $moduleUniqueID)
-    {
-        parent::__construct($moduleUniqueID);
-
-    }
-
-    /**
-     * Creates database structure according to models annotations
-     *
-     * If necessary, it fills some default settings, and change sidebar menu item representation for this module
-     *
-     * After installation, it registers module on PbxExtensionModules model
-     *
-     *
-     * @return bool result of installation
-     */
-    public function installDB(): bool
-    {
-        $result = $this->createSettingsTableByModelsAnnotations();
-
-        if ($result) {
-            $result = $this->registerNewModule();
-        }
-
-        if ($result) {
-            $result = $this->addToSidebar();
-        }
-
-        return $result;
-    }
-
-    /**
-     * Create folders on PBX system and apply rights
-     *
-     * @return bool result of installation
-     */
-    public function installFiles(): bool
-    {
-        return parent::installFiles();
-    }
-
-    /**
-     * Unregister module on PbxExtensionModules,
-     * Makes data backup if $keepSettings is true
-     *
-     * Before delete module we can do some soft delete changes, f.e. change forwarding rules i.e.
-     *
-     * @param  $keepSettings bool creates backup folder with module settings
-     *
-     * @return bool uninstall result
-     */
-    public function unInstallDB($keepSettings = false): bool
-    {
-        return parent::unInstallDB($keepSettings);
-    }
-
-    /**
      * Adds the module to the sidebar menu.
      *
      * @return bool The result of the addition process.
