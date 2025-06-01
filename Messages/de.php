980b14e8bb2aceaf844b1cccd9e5ce9e8df34221
Nikolay Beketov <nbek@miko.ru>
2025-06-01 13:52:32 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (152 of 152 strings)
    
    Translation: MIKOPBX/ModuleUsersUI
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersui/de/

--- Messages/de.php
+++ Messages/de.php
@@ -17,7 +17,7 @@ return [
  * You should have received a copy of the GNU General Public License along with this program.
  * If not, see <https://www.gnu.org/licenses/>.
  */
-    'repModuleUsersUI' => 'Systemzugriffskontrolle – %repesent%',
+    'repModuleUsersUI' => 'Systemzugriffskontrolle - %repesent%',
     'mo_ModuleModuleUsersUI' => 'Systemzugriffskontrolle ',
     'BreadcrumbModuleUsersUI' => 'Systemzugriffskontrolle ',
     'SubHeaderModuleUsersUI' => 'Erstellung von Zugriffsgruppen, Einschränkung von Rechten, Domänenautorisierung',
